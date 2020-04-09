61b9c534aa4dbe958df909bebaffdcff6d279687
CalebFetzer <819841@mystma.org>
2020-04-09 14:34:13 +0000

    Added top five movies [fixed]

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,6 +2,10 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("Waterboy");
+        System.out.println("Fast & Furious");
+        System.out.println("Iron Man");
+        System.out.println("Spiderman");
+        System.out.println("Grown ups 2");
     }
 }
