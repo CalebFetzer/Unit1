5388f4cd9664cdc16e1581b2e944231b5dadc55f
CalebFetzer <819841@mystma.org>
2020-04-13 15:40:16 +0000

    fixed mad lib

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -4,20 +4,26 @@ import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String person1 = Jack
-        String person2 = Jill
-        String verb1 = Run 
-        String verb2 = Walk
-        String verb3 = Jump
-        String food = Pizza
-        String dayOfWeek = Monday
-        String place1 = Target
-        String place2 = McDonalds
-        String place3 = My House
-        String adjective1 = Purple
-        String adjective2 = Round
+        String person1 = "Jack";
+        String person2 = "Jill";
+        String verb1 = "Running";
+        String verb2 = "Walked";
+        String verb3 = "Jumped";
+        String food = "Pizza";
+        String dayOfWeek = "Monday";
+        String place1 = "Target";
+        String place2 = "McDonalds";
+        String place3 = "Hospital";
+        String adjective1 = "Purple";
+        String adjective2 = "Round";
 
-        
+        System.out.println("On my trip to New York, I landed in Times Square. When I got there, I was hustled by" + " " + person1 + " and" + " " + person2);
+        System.out.println("While" + " " + verb1 + " " + "for" + " " + food + " " + "I" + " " + verb2 + " and found a place with more food options.");
+        System.out.println("The place was called" + " " + place1 + "." + " I went up to the door, and it said that it was closed on" + " " + dayOfWeek + ".");
+        System.out.println("So then I decided to find somewhere else to eat. I " + verb3 + " for a bit, until a stumbled across a big building called " + place3 + ".");
+        System.out.println("That clearly wasn't a restaraunt, so I walked a bit more and found a small" +" " +adjective1 + " " + adjective2 + " building called" + " " + place2 + ".");
+        System.out.println("I walked inside, and was able to get something to eat. The end!");
+
         
     }
 }
