69f0d37f055dc8f109e7f1cd64ebe04ed2d3b265
CalebFetzer <819841@mystma.org>
2020-04-13 15:00:21 +0000

    added mad lib

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,11 +1,23 @@
 package app;
 
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        String firstName = "Caleb";
-        String lastName = "Fetzer";
+        String person1 = Jack
+        String person2 = Jill
+        String verb1 = Run 
+        String verb2 = Walk
+        String verb3 = Jump
+        String food = Pizza
+        String dayOfWeek = Monday
+        String place1 = Target
+        String place2 = McDonalds
+        String place3 = My House
+        String adjective1 = Purple
+        String adjective2 = Round
 
-        System.out.println(firstName + " " + lastName);
+        
         
     }
 }
