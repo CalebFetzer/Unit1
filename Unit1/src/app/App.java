85160ad07935fd51fc68439b8618006b7ef251b2
CalebFetzer <819841@mystma.org>
2020-04-13 14:46:32 +0000

    added first and last name

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,22 +2,10 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        boolean livesInAlbertville = false;
-        char firstLetterOfName = 'C';
-        byte thisIsAByte = 123;
-        long veryLargeNum = 92233720368547758;
-        short notReallyShort = -31090;
+        String firstName = "Caleb";
+        String lastName = "Fetzer";
 
-
-
-        System.out.println("What is the first letter of my name?");
-        System.out.println(firstLetterOfName);
-        System.out.println("Do I live in Albertville?");
-        System.out.println(livesInAlbertville);
-        System.out.println(thisIsAByte);
-        System.out.println(veryLargeNum);
-        System.out.println(notReallyShort);
-
+        System.out.println(firstName + " " + lastName);
         
     }
 }
