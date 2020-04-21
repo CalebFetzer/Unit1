323bc6e87ff2bcf75151561e54e511441f3395b3
CalebFetzer <819841@mystma.org>
2020-04-21 18:52:14 +0000

    added what I have of If Statements

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,14 +3,27 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
+
+            System.out.println("Start First Loop");
+
         for (int i = 1; i <= 20; i++) {
-            if (i % 4 == 0) {
-                System.out.println(i + " is evenly divisible by four");
-            } else {
-                System.out.println(i + " is not evenly divisible by four");
+            if (i == 10){
+                int o = 1;
+            
+
+                    while (o <= 6){
+
+                    System.out.println(i);
+                    o++;
+
+                 }
+                    
+                }else {
+                    System.out.println(i);
+                }
+            }
+
+
+            
             }
         }
-
-        System.out.println("All finished!");
-    }
-}
