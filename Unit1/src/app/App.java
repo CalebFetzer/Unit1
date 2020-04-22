d820f772d6167ed252e7bc7579564a53c6ee0115
CalebFetzer <819841@mystma.org>
2020-04-22 14:51:27 +0000

    added if statements completed

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,27 +3,28 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-
-            System.out.println("Start First Loop");
-
-        for (int i = 1; i <= 20; i++) {
-            if (i == 10){
-                int o = 1;
+        System.out.println("Start first loop");
             
+        for (int i = 0; i < 20; i++){
 
-                    while (o <= 6){
-
-                    System.out.println(i);
-                    o++;
-
-                 }
-                    
-                }else {
+            System.out.println(i);
+            if (i == 10){
+                for (int j = 0; j < 5; j++){
                     System.out.println(i);
                 }
             }
+        }
+        System.out.println("End first loop");
+        System.out.println("Start second loop");
+        for (int i = 0; i <20; i = i + 2){
+            if(i % 3 == 0){
+                i = i - 1;
 
+            }
+            System.out.println(i);
+        }
 
-            
+        
+
             }
         }
