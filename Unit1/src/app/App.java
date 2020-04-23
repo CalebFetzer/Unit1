a391418358068a1c85dbc3414d5763b3908948de
CalebFetzer <819841@mystma.org>
2020-04-23 14:52:39 +0000

    added logic challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,30 +2,24 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Start first loop");
             
-        for (int i = 20; i < 0; i = i++){
+            System.out.println("Start first loop");
+            for (int i = 20; i > 0; i = i - 1){
+                System.out.println(i);
+                if ( i == 10){
+                    for (int j = 0; j < 7; j++){
                         System.out.println(i);
-
-            if (i == 10){
-                for (int j =5; j >5; j++)
-                    System.out.println(i);
+                    }
                 }
             }
-
-        System.out.println("End first loop");
-        System.out.println("Start second loop");
-        
-        for (int i = 0; i < 20; i = i + 12){
-            if (i + 12 == 0){
-                System.out.println("Hey!");
-            }
-            else{
+            System.out.println("End first loop");
+            System.out.println("Start second loop");
+            for (int i = 0; i < 37; i = i + 4){
                 System.out.println(i);
+                if (i % 12 == 0){
+                    System.out.println("Hey!");
+                }
             }
-
-        }
-        
-
-            }
-        }
+            System.out.println("End second loop");
+        }   
+    }
