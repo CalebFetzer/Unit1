bb27ea0b78be0c818a00375859e6f63db5628ec9
CalebFetzer <819841@mystma.org>
2020-04-23 15:42:45 +0000

    added Double or single digit challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,24 +2,25 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-            
-            System.out.println("Start first loop");
-            for (int i = 20; i > 0; i = i - 1){
-                System.out.println(i);
-                if ( i == 10){
-                    for (int j = 0; j < 7; j++){
-                        System.out.println(i);
-                    }
-                }
+
+        System.out.println("Double or single digit?");
+        for (int i = 20; i > 0; i = i - 1) {
+            if (i >= 10) {
+                System.out.println(i + " is a double digit");
+            } else {
+                System.out.println(i + " is a single digit");
             }
-            System.out.println("End first loop");
-            System.out.println("Start second loop");
-            for (int i = 0; i < 37; i = i + 4){
-                System.out.println(i);
-                if (i % 12 == 0){
-                    System.out.println("Hey!");
-                }
+        }
+        System.out.println("Nice!");
+        System.out.println("Fast Food");
+        for (int i = 1; i < 12; i = i + 1) {
+            if (i % 2 == 0) {
+                System.out.println("Fries");
+            } else {
+                System.out.println("Burgers");
             }
-            System.out.println("End second loop");
-        }   
+        }
+        System.out.println("Nice!");
+
     }
+}
