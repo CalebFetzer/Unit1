9ef207f0c8f6febfe7873fbbc0a6971e2c52fd73
CalebFetzer <819841@mystma.org>
2020-04-27 14:46:15 +0000

    I forgot to push this to github, sorry.

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,26 +1,29 @@
 package app;
 
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Double or single digit?");
-        for (int i = 20; i > 0; i = i - 1) {
-            if (i >= 10) {
-                System.out.println(i + " is a double digit");
-            } else {
-                System.out.println(i + " is a single digit");
+         System.out.println("Start first loop");
+        for (int i = 1; i < 10; i = i + 1) {
+            System.out.println(i);
+            if (i == 5) {
+                System.out.println("five");
             }
         }
-        System.out.println("Nice!");
-        System.out.println("Fast Food");
-        for (int i = 1; i < 12; i = i + 1) {
-            if (i % 2 == 0) {
-                System.out.println("Fries");
-            } else {
-                System.out.println("Burgers");
+        System.out.println("End first loop");
+        System.out.println("Start second loop");
+        for (int i = 100; i <= 200; i = i + 10) {
+            System.out.println(i);
+            if (i == 150){
+                for (int j = 150; j < 160; j = j + 1){
+                    System.out.println(j);
+                }
             }
-        }
-        System.out.println("Nice!");
+                } 
 
-    }
-}
+            }
+        }
+    
+
+
