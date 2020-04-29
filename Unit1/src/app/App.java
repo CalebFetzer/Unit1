eb4b56356c9b034680b8869092b0276fae482ddf
CalebFetzer <819841@mystma.org>
2020-04-29 15:39:48 +0000

    added password

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,21 +6,18 @@ public class App {
 
             Scanner input = new Scanner(System.in);
 
-            System.out.println("Please enter your first name.");
-            String name = input.nextLine();
-            System.out.println("Please enter your last name");
-            String lastName = input.nextLine();
-            System.out.println("Please enter your age");
-            String yourAge = input.nextLine();
-            System.out.println("Please enter your grade");
-            String yourGrade = input.nextLine();
-            System.out.println("Please enter your favorite color");
-            String favoriteColor = input.nextLine();
+            System.out.println("Please enter your password.");
+            String passWord = input.nextLine();
 
-            System.out.println("Hello," + name + " " + lastName);
-            System.out.println("You are " + yourAge + " years old");
-            System.out.println("You are in " + yourGrade + "th grade");
-            System.out.println("Your favorite color is" + favoriteColor);
+            if (passWord.equals("183726073")){
+                System.out.println("Access granted");
+            }else{
+                System.out.println("Access denied");
+            }
+            
+            
+
+
 
             }
         }
