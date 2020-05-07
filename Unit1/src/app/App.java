9c51795b71a3b18d2cc08c332f035cba4c7eb9cd
CalebFetzer <819841@mystma.org>
2020-05-07 15:29:31 +0000

    added input mad lib

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -7,32 +7,40 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("How many continents are there?");
-        String continents = input.nextLine();
+        System.out.println("Please Enter a noun");
+        String noun = input.nextLine();
+       
+        System.out.println("Please enter a name");
+        String name = input.nextLine();
 
-        if (continents.equals("7")){
-            System.out.println("Corrrect");
-        }else{
-            System.out.println("Incorrect");
-        }
-        System.out.println("How many hours are in a day?");
-        String hrsDay = input.nextLine();
+        System.out.println("Please enter a verb");
+        String verb = input.nextLine();
 
-        if (hrsDay.equals("24")){
-            System.out.println("Correct");
-        }else{
-            System.out.println("Incorrect");
-        }
-        System.out.println("What is 1+1?");
-        String correctNum = input.nextLine();
-        if (correctNum.equals("2")){
-            System.out.println("Correct");
-        }else {
-            System.out.println("Incorrect");
-        }
+        System.out.println("Please enter an adjective");
+        String adjective = input.nextLine();
 
+        System.out.println("Please enter a place");
+        String place = input.nextLine();
 
-        
+        System.out.println("Please enter a food");
+        String food = input.nextLine();
+
+        System.out.println("Please enter an animal");
+        String animal = input.nextLine();
+
+        System.out.println("Please enter a number");
+        String num = input.nextLine();
+
+         System.out.println("James went to the store and was going to buy a " + noun);
+         System.out.println("He then grabbed it, and proceeded to pay for his items. That's where he found a guy named " + name);
+         System.out.println("He then decided to" + verb + " away");
+         System.out.println("He got in his" + " car, and drove away.");
+         System.out.println("He didn't know where to go, so he decided to drive to " + place);
+         System.out.println("When he got there, he got hungry so he went and got food. He chose to order a " + food);
+         System.out.println("He got his food, and left. When he got outside of " + place + ", he saw a "+ animal);
+         System.out.println("At that point, he realized that he was super tired, and decided to drive home on Highway " + num);
+         System.out.println("The end!");
+
         
 
     }
