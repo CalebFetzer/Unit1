e1e866c3fa898553695b9ae49835bd440082a1b9
CalebFetzer <819841@mystma.org>
2020-05-19 15:04:02 +0000

    added more person classes

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -8,6 +8,9 @@ public class App {
         Dog dog1 = new Dog();
         Dog dog2 = new Dog();
         Dog dog3 = new Dog();
+        Person person1 = new Person();
+        Person person2 = new Person();
+        Person person3 = new Person();
 
         Scanner input = new Scanner(System.in);
 
@@ -15,9 +18,16 @@ public class App {
         dog2.breed = "Black Lab";
         dog3.age = 2;
 
+        person1.iq = 9;
+        person2.shoeSize = 10;
+        person3.height = "5'6";
+
         System.out.println(dog1.name);
         System.out.println(dog2.breed);
         System.out.println(dog3.age);
+        System.out.println(person1.iq);
+        System.out.println(person2.shoeSize);
+        System.out.println(person3.height);
 
     }
 }
--- Unit1/src/app/Person.java
+++ Unit1/src/app/Person.java
@@ -5,5 +5,7 @@ public class Person{
 String name;
 int age;
 String eyeColor;
-
+int iq;
+int shoeSize;
+String height;
 }
